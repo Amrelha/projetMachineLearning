00b7a152b447097d604451ef8029a4e86861171e
Amrelha <59119177+Amrelha@users.noreply.github.com>
2020-06-09 18:02:17 +0100

    commit3

--- pythonBackend/venv/Include/dataExtraction.py
+++ pythonBackend/venv/Include/dataExtraction.py
@@ -1,3 +1,35 @@
 import pandas as pd
+from bs4 import BeautifulSoup as soup
+from urllib.request import urlopen, Request
+from flask import jsonify
+#change the url
+url ="C:/Users/elham/Desktop/S4/Maching learning/projet machine learning/covid-19-master/data/countries-aggregated.csv"
+dataset = pd.read_csv(url)
 
-url =""
+# typeofdata : Confirmed, Recovered, Deaths
+def getData(typeofdata ,countryName):#
+    data = dataset[['Country', typeofdata]]
+    data = data.groupby('Country')
+    array = data.get_group(countryName)[typeofdata].values
+    return array
+def getMaxOfAll(country):
+    data = dataset[['Country', 'Confirmed', 'Recovered', 'Deaths']]
+    data = data.groupby(['Country']).max()
+    print(data.loc[country])
+    data = data.loc[country].values
+    print(data)
+    return data
+def getNewData(country):
+    url = "https://www.worldometers.info/coronavirus/country/"+country
+    req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    page_byte = urlopen(req).read()
+    webpage = page_byte.decode('utf-8')
+    page_soupe = soup(webpage, "html.parser")
+    containers = page_soupe.findAll("div", {"class": "maincounter-number"})
+    data = []
+    for i in range(0, 3):
+        data.append(containers[i].span.text)
+    return data
+
+if __name__ == "__main__":
+    getNewData("morocco")
--- pythonBackend/venv/Include/service.py
+++ pythonBackend/venv/Include/service.py
@@ -1,22 +1,32 @@
 from flask import Flask, jsonify
+import dataExtraction as dataEx
 
 app = Flask(__name__)
 
 # Visualisation service part
 @app.route('/visualisation/confirmed/<string:country>')
 def confirmedCases(country):
-    tab = []
-    tab.append(5)
-    tab.append(6)
-    return jsonify({"tab" : tab})
+    array = dataEx.getData("Confirmed",country).tolist()
+    return jsonify({"confirmed" : array})
 
 @app.route('/visualisation/recovered/<string:country>')
 def recoveredCases(country):
-    return jsonify({"test":"recovered "+country})
+    array = dataEx.getData("Recovered", country).tolist()
+    return jsonify({"recovered": array})
 
 @app.route('/visualisation/death/<string:country>')
 def deathCases(country):
-    return jsonify({"test":"death "+country})
+    array = dataEx.getData("Deaths", country).tolist()
+    return jsonify({"deaths": array})
+@app.route('/visualisation/maxofall/<string:country>')
+def maxofall(country):
+    array = dataEx.getMaxOfAll(country).tolist()
+    return jsonify({"confirmed" : array[0], "recovered" : array[1], "death" : array[2]})
+@app.route('/visualisation/newdata/<string:country>')
+def NewData(country):
+    array = dataEx.getNewData(country)
+    return jsonify({"totalCases" : array[0], "death" : array[1], "recovered" : array[2]})
+
 
 if __name__ == "__main__":
     app.run(debug=True)
