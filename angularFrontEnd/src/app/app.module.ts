c5ccdd0e46f1838c6eda8981ac2810a379557bcd
Amrelha <59119177+Amrelha@users.noreply.github.com>
2020-06-10 00:25:46 +0100

    commit4

--- angularFrontEnd/src/app/app.module.ts
+++ angularFrontEnd/src/app/app.module.ts
@@ -7,7 +7,9 @@ import { AccueilComponent } from './accueil/accueil.component';
 import { VisualisationComponent } from './visualisation/visualisation.component';
 import { PredictComponent } from './predict/predict.component';
 import { AnalyseSentimentComponent } from './analyse-sentiment/analyse-sentiment.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,7 +20,10 @@ import { AnalyseSentimentComponent } from './analyse-sentiment/analyse-sentiment
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MDBBootstrapModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
