6aa76d45443804aa20d2bea2dab450889519f88b
issamlf1 <55587212+issamlf1@users.noreply.github.com>
2020-06-09 22:22:16 +0100

    v6

---
+++ angularFrontEnd/src/app/analyse-sentiment/analyse-sentiment.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-analyse-sentiment',
+  templateUrl: './analyse-sentiment.component.html',
+  styleUrls: ['./analyse-sentiment.component.scss']
+})
+export class AnalyseSentimentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- angularFrontEnd/src/app/app.module.ts
+++ angularFrontEnd/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { VisualisationComponent } from './visualisation/visualisation.component';
+import { PredictComponent } from './predict/predict.component';
+import { AnalyseSentimentComponent } from './analyse-sentiment/analyse-sentiment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccueilComponent,
+    VisualisationComponent,
+    PredictComponent,
+    AnalyseSentimentComponent
   ],
   imports: [
     BrowserModule,
