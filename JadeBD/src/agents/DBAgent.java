637f6f459306e173802551fea5cdb6b7bf151f53
issamlf1 <55587212+issamlf1@users.noreply.github.com>
2020-07-05 20:44:57 +0100

    v27

--- JadeBD/src/agents/DBAgent.java
+++ JadeBD/src/agents/DBAgent.java
@@ -9,6 +9,7 @@ import com.mongodb.DBCollection;
 import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
+import com.mongodb.util.JSON;
 
 import jade.core.Agent;
 import jade.core.behaviours.CyclicBehaviour;
@@ -29,110 +30,63 @@ public class DBAgent extends Agent{
 				MessageTemplate msgTemplate_economy= MessageTemplate.MatchOntology("economy");
 				MessageTemplate msgTemplate_politics = MessageTemplate.MatchOntology("politics");
 				MessageTemplate msgTemplate_Mhealth= MessageTemplate.MatchOntology("mentalhealth");
+				MessageTemplate msgTemplate_ClusteringTest= MessageTemplate.MatchOntology("clusteringTest");
+				MessageTemplate msgTemplate_ClusteringAge= MessageTemplate.MatchOntology("clusteringAge");
 				ACLMessage nature_message = receive(msgTemplate_nature);
 				ACLMessage economy_message = receive(msgTemplate_economy);
 				ACLMessage politics_message = receive(msgTemplate_politics);
 				ACLMessage Mhealth_message = receive(msgTemplate_Mhealth);
+				ACLMessage clusterTest_message = receive(msgTemplate_ClusteringTest);
+				ACLMessage clusterAge_message = receive(msgTemplate_ClusteringAge);
 				
 				if(nature_message != null) {
-					Map<String, Integer> data = new HashMap<>();
 					String result = nature_message.getContent();
-					result =result.replaceAll("\\s","");  
-					result=result.replaceAll("\\{", "");
-					result= result.replaceAll("\\}", "");
-					result=result.replaceAll("\"", "");
-					
-					String [] table = result.split(":|,");
-					
-					for (int i = 0; i < table.length; i++) {
-						System.out.println(table[i]);
-						
-					}
-					for (int j = 0; j < table.length; j+=2) {
-						data.put(table[j], Integer.valueOf(table[j+1]));
-					}
-					
 					MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb://localhost:27017"));
 					DB database = mongoClient.getDB("scrapingDB");
 					DBCollection collection = database.getCollection("nature");
-					DBObject nature = new BasicDBObject(data);
-                            
-					
-					collection.insert(nature);
+					DBObject dbObject = (DBObject) JSON.parse(result);
+					collection.insert(dbObject);
 				}
 				else if(economy_message != null) {
-					Map<String, Integer> data = new HashMap<>();
 					String result = economy_message.getContent();
-					result =result.replaceAll("\\s","");  
-					result=result.replaceAll("\\{", "");
-					result= result.replaceAll("\\}", "");
-					result=result.replaceAll("\"", "");
-					
-					String [] table = result.split(":|,");
-					
-					for (int i = 0; i < table.length; i++) {
-						System.out.println(table[i]);
-						
-					}
-					for (int j = 0; j < table.length; j+=2) {
-						data.put(table[j], Integer.valueOf(table[j+1]));
-					}
-					
 					MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb://localhost:27017"));
 					DB database = mongoClient.getDB("scrapingDB");
 					DBCollection collection = database.getCollection("economy");
-					DBObject economy = new BasicDBObject(data);
-					
-					collection.insert(economy);
+					DBObject dbObject = (DBObject) JSON.parse(result);
+					collection.insert(dbObject);
 				}
 				else if (politics_message != null) {
-					Map<String, Integer> data = new HashMap<>();
 					String result = politics_message.getContent();
-					result =result.replaceAll("\\s","");  
-					result=result.replaceAll("\\{", "");
-					result= result.replaceAll("\\}", "");
-					result=result.replaceAll("\"", "");
-					
-					String [] table = result.split(":|,");
-					
-					for (int i = 0; i < table.length; i++) {
-						System.out.println(table[i]);
-						
-					}
-					for (int j = 0; j < table.length; j+=2) {
-						data.put(table[j], Integer.valueOf(table[j+1]));
-					}
-					
 					MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb://localhost:27017"));
 					DB database = mongoClient.getDB("scrapingDB");
 					DBCollection collection = database.getCollection("politics");
-					DBObject politic = new BasicDBObject(data);
+					DBObject dbObject = (DBObject) JSON.parse(result);
 					
-					collection.insert(politic);
+					collection.insert(dbObject);
 				}
 				else if (Mhealth_message != null) {
-					Map<String, Integer> data = new HashMap<>();
 					String result = Mhealth_message.getContent();
-					result =result.replaceAll("\\s","");  
-					result=result.replaceAll("\\{", "");
-					result= result.replaceAll("\\}", "");
-					result=result.replaceAll("\"", "");
-					
-					String [] table = result.split(":|,");
-					
-					for (int i = 0; i < table.length; i++) {
-						System.out.println(table[i]);
-						
-					}
-					for (int j = 0; j < table.length; j+=2) {
-						data.put(table[j], Integer.valueOf(table[j+1]));
-					}
-					
 					MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb://localhost:27017"));
 					DB database = mongoClient.getDB("scrapingDB");
 					DBCollection collection = database.getCollection("mentalhealth");
-					DBObject health = new BasicDBObject(data);
-					collection.insert(health);
+					DBObject dbObject = (DBObject) JSON.parse(result);
+					collection.insert(dbObject);
+				}
+				else if (clusterTest_message != null) {
+					String result = clusterTest_message.getContent();
+					MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb://localhost:27017"));
+					DB database = mongoClient.getDB("ClusteringDB");
+					DBCollection collection = database.getCollection("clusteringTest");
+					DBObject dbObject = (DBObject) JSON.parse(result);
+					collection.insert(dbObject);
+				}
+				else if (clusterAge_message != null) {
+					String result = clusterAge_message.getContent();
+					MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb://localhost:27017"));
+					DB database = mongoClient.getDB("ClusteringDB");
+					DBCollection collection = database.getCollection("clusteringAge");
+					DBObject dbObject = (DBObject) JSON.parse(result);
+					collection.insert(dbObject);
 				}
 				else {
 					block();
